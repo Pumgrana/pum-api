308341de0a6caae438cfdbec935070661a8667db
Tristan <djahel.tristan@protonmail.com>
2017-05-23 20:14:09 +0200

    Email fourni par devise

--- app/controllers/plugin_controller.rb
+++ app/controllers/plugin_controller.rb
@@ -7,7 +7,7 @@ class PluginController < ApplicationController
     leave_time = params[:leave_time]
     focus_elasped_time = params[:focus_elasped_time]
     total_elasped_time = params[:total_elasped_time]
-    history = :History.create(email: params[:email],
+    history = History.create(email: current_user.email,
                               url: params[:url],
                               origin_url: params[:origin_url],
                               target_url: params[:target_url],
