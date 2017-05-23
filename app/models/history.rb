c6cf1aa6a5c9118ce6ecf77d6c836068fbea0138
Tristan <djahel.tristan@protonmail.com>
2017-05-23 21:24:16 +0200

    remove email

--- app/models/history.rb
+++ app/models/history.rb
@@ -7,7 +7,6 @@ class History < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   mapping do
-    indexes :email, index: 'not_analyzed'
     indexes :url, index: 'not_analyzed'
     indexes :origin_url, index: 'not_analyzed'
     indexes :target_url, index:'not_analyzed'
